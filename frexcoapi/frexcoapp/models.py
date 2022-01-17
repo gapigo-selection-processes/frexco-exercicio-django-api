e59e9a2a3696de838e3f4193cf96dffd64eb216b
gapigo <x.gabriel12@gmail.com>
2022-01-17 17:14:36 -0300

    Adição dos models e renomeação do app principal

---
+++ frexcoapi/frexcoapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=30)
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=30)
+    region = models.ForeignKey('Region', on_delete=models.CASCADE)
